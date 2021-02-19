d10cca709ee59bf9f3bc3a4de9216253575058c8
mrahman4782 <moharahman50@gmail.com>
2021-02-18 23:40:23 -0500

    First commit

--- app/src/main/java/com/example/unit_1_codepath/MainActivity.java
+++ app/src/main/java/com/example/unit_1_codepath/MainActivity.java
@@ -23,8 +23,8 @@ import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String Playing = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
-    public static final String Tag = "MainActivity";
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
 
     List<Movie> movies;
 
@@ -35,58 +35,43 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView rvMovies = findViewById(R.id.rvMovies);
         movies = new ArrayList<>();
 
+
         final MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+
         rvMovies.setAdapter(movieAdapter);
+
+
         rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
-
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get(Playing, new JsonHttpResponseHandler() {
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Headers headers, JSON json) {
-                Log.d(Tag, "onSuccess");
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
                 JSONObject jsonObject = json.jsonObject;
+
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
-                    Log.i(Tag, "Results:" + results.toString());
+                    Log.i(TAG, "Results: " + results.toString());
                     movies.addAll(Movie.fromJsonArray(results));
                     movieAdapter.notifyDataSetChanged();
-                    Log.i(Tag, "Movies: " + movies.size());
+                    Log.i(TAG, "Movies:" + movies.size());
                 } catch (JSONException e) {
-                    Log.e(Tag, "Hit json exception", e);
-
+                    Log.e(TAG, "Hit Json exception", e);
+                    e.printStackTrace();
                 }
+
             }
 
             @Override
-            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.d(Tag, "onFailure");
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure");
             }
         });
 
-        /*
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(Playing, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Headers headers, JSON json) {
-                Log.d(Tag, "Results");
-                JSONObject jsonObject = json.jsonObject;
-                try{
-                    JSONArray results = jsonObject.getJSONArray(name: "results");
-                    Log.i(Tag, msg: "Results: " + results.toString());
-
-                }
-                catch (JSONException e){
-                    Log.e(Tag, msg: "Hit json exception", e);
-                }
-            }
-            @Override
-            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable){
-                Log.d(Tag, msg: "onFailure");
-            }
-        });
- */
     }
 
-}
 
+
+}
--- app/src/main/java/com/example/unit_1_codepath/models/Movie.java
+++ app/src/main/java/com/example/unit_1_codepath/models/Movie.java
@@ -3,32 +3,52 @@ package com.example.unit_1_codepath.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Parcel
 public class Movie {
+
+    int movieId;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
 
-    public Movie(JSONObject jsonObject) throws JSONException{
+    // empty constructor is necessary for Parceler Library
+    public Movie(){
+
+    }
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
-        List<Movie> movies = new ArrayList<>();
-        for (int i = 0; i <movieJsonArray.length(); i++){
+        List<Movie> movies = new ArrayList();
+        for (int i = 0; i <movieJsonArray.length(); i++ ) {
             movies.add(new Movie(movieJsonArray.getJSONObject(i)));
         }
         return movies;
+
     }
 
     public String getPosterPath() {
+
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getBackdropPath(){
+
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
 
     public String getTitle() {
@@ -37,5 +57,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public int getMovieId(){
+        return movieId;
     }
 }
